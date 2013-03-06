201ebb1a1ab490700db501c0531749e9a02f108e
Matthias Kretz <kretz@kde.org>
2013-03-06 19:22:23 +0100

    add switch to disable the use of rdtscp

--- tsc.h
+++ tsc.h
@@ -43,8 +43,10 @@ class TimeStampCounter
 inline void TimeStampCounter::Start()
 {
 #ifdef _MSC_VER
-	unsigned int tmp;
+    unsigned int tmp;
     m_start.a = __rdtscp(&tmp);
+#elif defined NO_RDTSCP
+    asm volatile("xor %%eax,%%eax\n\tcpuid\n\trdtsc" : "=a"(m_start.b[0]), "=d"(m_start.b[1]) :: "ebx", "ecx" );
 #else
     asm volatile("rdtscp" : "=a"(m_start.b[0]), "=d"(m_start.b[1]) :: "ecx" );
 #endif
@@ -53,8 +55,10 @@ inline void TimeStampCounter::Start()
 inline void TimeStampCounter::Stop()
 {
 #ifdef _MSC_VER
-	unsigned int tmp;
+    unsigned int tmp;
     m_end.a = __rdtscp(&tmp);
+#elif defined NO_RDTSCP
+    asm volatile("xor %%eax,%%eax\n\tcpuid\n\trdtsc" : "=a"(m_end.b[0]), "=d"(m_end.b[1]) :: "ebx", "ecx" );
 #else
     asm volatile("rdtscp" : "=a"(m_end.b[0]), "=d"(m_end.b[1]) :: "ecx" );
 #endif
